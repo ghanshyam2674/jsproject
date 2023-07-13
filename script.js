7c1b151d6c8152df2f199b9565a05465c7c1a7d0
ghanshyam2674 <ghanshyamsharmaji24@gmail.com>
2023-07-13 20:04:41 +0530

    sdfsdf

---
+++ script.js
@@ -0,0 +1,37 @@
+let division2 = document.getElementById("division2")
+
+let tringle = () => {
+    b = 200;
+    h = 200;
+    a = (1 / 2) * b * h;
+    division2.innerHTML = a;
+}
+
+
+let percentages = () => {
+    hindi = 100;
+    english = 50;
+    maths = 30;
+    science = 20;
+    evs = 40;
+    total = (hindi + english + maths + science);
+    per = (total / 5)
+    division2.innerHTML = per;
+}
+
+
+let  simple=()=>{
+    p = 40000
+    r =1.5
+    t=1  
+    si = (p*r*t)/100
+    division2.innerHTML = si
+}
+
+
+let compound=()=>{
+    p =40000
+    r = 4
+    t= 23
+    
+}
